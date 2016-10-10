0c8520e4d3b4d74e6d4e6ef08c296e80b8537821
Aalex Gabi <aalexgabi@gmail.com>
2016-10-10 10:52:02 +0200

    Added base canvas

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById('mainScene');
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "rgb(200,0,0)"; // sets the color to fill in the rectangle with
+ctx.fillRect(10, 10, 55, 50);
