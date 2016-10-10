d7d0ebe7391cdc008635dc0c0fc9eb491fe42a33
Aalex Gabi <aalexgabi@gmail.com>
2016-10-10 11:16:19 +0200

    Added rendering system

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,31 @@
 var canvas = document.getElementById('mainScene');
 var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "rgb(200,0,0)"; // sets the color to fill in the rectangle with
-ctx.fillRect(10, 10, 55, 50);
+var renderInterval = 1000;
+
+var white = "rgb(255,255,255)";
+var red = "rgb(200,0,0)";
+
+function fillRect(color, x, y, width, height) {
+    // Sets the color to fill in the rectangle with
+    ctx.fillStyle = color;
+    // Creates fill
+    ctx.fillRect(x , y, width, height);
+}
+
+var x = 0;
+
+function render() {
+    // Call itself again to continue rendering according to game speed
+    setTimeout(render, renderInterval);
+
+    fillRect(red, 10 * x, 10, 10, 10);
+    fillRect(white, 10 * (x - 1), 10, 10, 10);
+
+    // TODO: increase speed with each frame
+
+    x++;
+}
+
+// Render first frame
+render();
